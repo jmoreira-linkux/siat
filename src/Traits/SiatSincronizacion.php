b7323ddba1bd922c62ec975d775cdcbcae8c8539
Jorge Moreira <jmoreira@linkux-it.com>
2021-12-04 00:35:41 -0400

    Resolve #9 sincronizacion de fecha hora

--- src/Traits/SiatSincronizacion.php
+++ src/Traits/SiatSincronizacion.php
@@ -20,6 +20,36 @@ trait SiatSincronizacion
         return $this->sincronizacionClient;
     }
 
+    /**
+     * Este proceso tiene como objetivo sincronizar la fecha y hora del Sistema de Facturación del Contribuyente
+     * con los datos proporcionadas por el SIN.
+     *
+     * @param string $cuis
+     * @return string Format 2021-12-04T00:19:57.987
+     */
+    public function sincronizarFechaHora($cuis)
+    {
+        $client = $this->getSincronizacionClient();
+        $response = $client->sincronizarFechaHora([
+            'SolicitudSincronizacion' => [
+                'codigoAmbiente' => $this->codigoAmbiente,
+                'codigoSistema' => $this->codigoSistema,
+                'nit' => $this->nit,
+                'cuis' => $cuis,
+                'codigoSucursal' => $this->codigoSucursal,
+                'codigoPuntoVenta' => $this->codigoPuntoVenta,
+            ],
+        ]);
+        return $response->RespuestaFechaHora->fechaHora;
+    }
+
+    /**
+     * Método por el cual el Sistema Informático de Facturación autorizado realiza la sincronización del catálogo de
+     * países que deben incluirse en las Facturas correspondientes.
+     *
+     * @param string $cuis
+     * @return array<mixed> stdClass([codigoClasificador] => int, [descripcion] => string)
+     */
     public function sincronizarParametricaPaisOrigen($cuis)
     {
         $client = $this->getSincronizacionClient();
@@ -36,6 +66,13 @@ trait SiatSincronizacion
         return $response->RespuestaListaParametricas->listaCodigos;
     }
 
+    /**
+     * Método por el cual el Sistema Informático de Facturación autorizado realiza la sincronización del catálogo de
+     * unidades de medida que deben incluirse en las Facturas correspondientes.
+     *
+     * @param string $cuis
+     * @return array<mixed> stdClass([codigoClasificador] => int, [descripcion] => string)
+     */
     public function sincronizarParametricaUnidadMedida($cuis)
     {
         $client = $this->getSincronizacionClient();
