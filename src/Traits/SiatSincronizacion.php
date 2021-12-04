9313a0fe5b39084eee66f026b1f2264fdb137b23
Jorge Moreira <jmoreira@linkux-it.com>
2021-12-04 00:00:27 -0400

    Resolve #4 sincronizacion de paises

---
+++ src/Traits/SiatSincronizacion.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Enors\Siat\Traits;
+
+trait SiatSincronizacion
+{
+    private function getSincronizacionClient()
+    {
+        if (!isset($this->sincronizacionClient)) {
+            $opts = [
+                'http' => [
+                    'header' => 'Authorization: Token ' . $this->token
+                ]
+            ];
+            $context = stream_context_create($opts);
+            $this->sincronizacionClient = new \SoapClient(self::SIAT_SINCRONIZACION_WSDL, [
+                'stream_context' => $context
+            ]);
+        }
+        return $this->sincronizacionClient;
+    }
+
+    public function sincronizarParametricaPaisOrigen($cuis)
+    {
+        $client = $this->getSincronizacionClient();
+        $response = $client->sincronizarParametricaPaisOrigen([
+            'SolicitudSincronizacion' => [
+                'codigoAmbiente' => $this->codigoAmbiente,
+                'codigoSistema' => $this->codigoSistema,
+                'nit' => $this->nit,
+                'cuis' => $cuis,
+                'codigoSucursal' => $this->codigoSucursal,
+                'codigoPuntoVenta' => $this->codigoPuntoVenta,
+            ],
+        ]);
+        return $response->RespuestaListaParametricas->listaCodigos;
+    }
+}
---
+++ tests/SiatSincronizacionTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Enors\Siat;
+
+use Dotenv\Dotenv;
+use PHPUnit\Framework\TestCase;
+
+class SiatSincronizacionTest extends TestCase
+{
+    private static $siat;
+    private static $cuis;
+
+    public static function setUpBeforeClass(): void
+    {
+        $credentials = new \stdClass;
+        $credentials->username = $_ENV['USERNAME'];
+        $credentials->password = $_ENV['PASSWORD'];
+        $auth = new Auth($_ENV['NIT'], $credentials);
+        $accessToken = $auth->getAccessToken();
+        self::$siat = new Siat($_ENV['CODIGO_SISTEMA'], $_ENV['NIT'], $accessToken);
+        self::$cuis = self::$siat->solicitarCUIS()->codigo;
+    }
+
+    public function testSincronizarParametricaPaisOrigen()
+    {
+        $response = self::$siat->sincronizarParametricaPaisOrigen(self::$cuis);
+        $this->assertGreaterThan(0, count($response));
+        $this->assertIsInt($response[0]->codigoClasificador);
+        $this->assertIsString($response[0]->descripcion);
+    }
+}
