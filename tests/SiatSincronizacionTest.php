b1ec7ba95b273f4bab693eaa7d3ab2161a0ac300
Lizeth Quispe <quismam@gmail.com>
2021-12-08 11:12:07 -0400

    resolve #14 sincronizacion leyendas facturas

--- tests/SiatSincronizacionTest.php
+++ tests/SiatSincronizacionTest.php
@@ -43,4 +43,10 @@ class SiatSincronizacionTest extends TestCase
         $this->assertIsInt($response[0]->codigoClasificador);
         $this->assertIsString($response[0]->descripcion);
     }
+
+    public function testSincronizarListaLeyendasFactura()
+    {
+        $response = self::$siat->sincronizarListaLeyendasFactura(self::$cuis);
+        $this->assertGreaterThan(0, count($response));
+    }
 }
