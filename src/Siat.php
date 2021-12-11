ddf69bd8211c4dcb72c19e62ceff06c209324cb5
Lizeth Quispe <quismam@gmail.com>
2021-12-10 20:10:51 -0400

    resolve #25 consulta punto venta

--- src/Siat.php
+++ src/Siat.php
@@ -53,56 +53,6 @@ class Siat
         $this->codigoPuntoVenta = $codigoPuntoVenta;
     }
 
-<<<<<<< HEAD
-=======
-    private function getCodigosClient()
-    {
-        if (!isset($this->codigosClient)) {
-            $opts = [
-                'http' => [
-                    'header' => 'Authorization: Token ' . $this->token
-                ]
-            ];
-            $context = stream_context_create($opts);
-            $this->codigosClient = new SoapClient(self::SIAT_CODIGOS_WSDL, ['stream_context' => $context]);
-        }
-        return $this->codigosClient;
-    }
-
-    public function solicitarCUIS()
-    {
-        $client = $this->getCodigosClient();
-        $response = $client->solicitudCuis([
-            'SolicitudOperacionesCuis' => [
-                'codigoAmbiente' => $this->codigoAmbiente,
-                'codigoSistema' => $this->codigoSistema,
-                'nit' => $this->nit,
-                'codigoModalidad' => $this->codigoModalidad,
-                'codigoSucursal' => $this->codigoSucursal,
-                'codigoPuntoVenta' => $this->codigoPuntoVenta,
-            ],
-        ]);
-        return $response->RespuestaCuis;
-    }
-
-    public function solicitarCUFD(string $cuis)
-    {
-        $client = $this->getCodigosClient();
-        $response = $client->solicitudCufd([
-            'SolicitudOperaciones'=>[
-            'codigoAmbiente' => $this->codigoAmbiente,
-            'codigoSistema' => $this->codigoSistema,
-            'nit' => $this->nit,
-            'codigoModalidad' => $this->codigoModalidad,
-            'cuis' => $cuis,
-            'codigoSucursal' => $this->codigoSucursal,
-            'codigoPuntoVenta' => $this->codigoPuntoVenta,
-            ]
-        ]);
-        return $response->RespuestaCufd;
-    }
-
->>>>>>> 93a4328 (resolve #13 solicitud cufd)
     public function generarCUF(
         int $timestamp,
         int $nroFactura,
